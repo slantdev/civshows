d73fcf0459d6be4fd0231c09c5421868f74e5761
Slant Dev <build@amazeworx.com>
2026-01-27 11:31:25 +0700

    feat: Change bg color on fallback image

--- template-parts/shows/components/exhibitors.php
+++ template-parts/shows/components/exhibitors.php
@@ -177,7 +177,7 @@ if (!is_wp_error($parent_terms)) {
                 <img src="<?php echo esc_url($logo_array['url']); ?>" alt="<?php the_title_attribute(); ?>" class="w-full h-full object-contain transition-transform hover:scale-105 duration-500">
               <?php else : ?>
                 <!-- Fallback Image -->
-                <div class="w-full h-full bg-black flex items-center justify-center text-white/50">
+                <div class="w-full h-full bg-gray-500 flex items-center justify-center text-white/50">
                   <svg xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                   </svg>
