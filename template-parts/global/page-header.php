35b8764a3535d40f10e75eeb4543123303151a3c
Slant Dev <build@amazeworx.com>
2026-02-18 10:45:23 +0700

    fix: refactor page-headers for pages and shows

--- template-parts/global/page-header.php
+++ template-parts/global/page-header.php
@@ -80,7 +80,7 @@ $parent_nav_label = $title_text;
 
 // Fetch Children Posts
 $child_args = [
-  'post_type'      => 'pages',
+  'post_type'      => 'page',
   'post_parent'    => $parent_id,
   'posts_per_page' => -1,
   'orderby'        => 'menu_order',
